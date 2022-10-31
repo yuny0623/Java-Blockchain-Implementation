062cd7213eeeda3ba8681f102b8cd35acc5f0b38
yuny0623 <overman522@gmail.com>
2022-10-31 10:03:50 +0900

    Test: add block creation test.

--- src/main/java/org/simplecoin/SimpleChain.java
+++ src/main/java/org/simplecoin/SimpleChain.java
@@ -2,13 +2,5 @@ package org.simplecoin;
 
 public class SimpleChain {
     public static void main(String[] args) {
-        Block genesisBlock = new Block("first block", "0");
-        System.out.println("Hash for block 1: " + genesisBlock.hash);
-
-        Block secondBlock = new Block("second block", genesisBlock.hash);
-        System.out.println("Hash for block 2: " + secondBlock.hash);
-
-        Block thirdBlock = new Block("thrid block", secondBlock.hash);
-        System.out.println("Hash for block 3: " + thirdBlock.hash);
     }
 }
---
+++ src/test/java/SimpleChainTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.simplecoin.Block;
+
+public class SimpleChainTest {
+
+    @Test
+    public void 블록테스트(){
+        // given
+        Block genesisBlock = new Block("first block", "0");
+        Block secondBlock = new Block("second block", genesisBlock.hash);
+        Block thirdBlock = new Block("thrid block", secondBlock.hash);
+
+        // when
+        System.out.println("Hash for block 1: " + genesisBlock.hash);
+        System.out.println("Hash for block 2: " + secondBlock.hash);
+        System.out.println("Hash for block 3: " + thirdBlock.hash);
+
+        // then
+        Assertions.assertNotNull(genesisBlock.hash);
+        Assertions.assertNotNull(secondBlock.hash);
+        Assertions.assertNotNull(thirdBlock.hash);
+    }
+}
