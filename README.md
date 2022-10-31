6879957448fd4f1396308d01748b39647f02ab61
yuny0623 <overman522@gmail.com>
2022-10-31 09:30:57 +0900

    Add: Block class which represent block.

---
+++ README.md
@@ -0,0 +1,12 @@
+simplecoin blockchain implementation with Java 
+-----------------------------------------------
+
+2022-10-30 ~ 
+
+아래 첨부된 포스팅의 예제와 설명을 참고하여 자바로 구현된 블록체인을 구현해보았습니다. 
+블록체인의 기본적인 개념을 구현하고 간단한 작업 증명 시스템을 구현해봅니다. 
+
+Reference:
+- https://medium.com/programmers-blockchain
+
+
--- src/main/java/org/simplecoin/Block.java
+++ src/main/java/org/simplecoin/Block.java
@@ -1,2 +1,17 @@
-package org.simplecoin;public class Block {
+package org.simplecoin;
+
+import java.util.Date;
+
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+
+    public Block(String data, String previousHash){
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+    }
 }
