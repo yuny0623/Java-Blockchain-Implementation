1b4ef907727086e1ae39d57e05690ce318e52022
yuny0623 <overman522@gmail.com>
2022-10-31 10:45:33 +0900

    Add: mining method added. also difficulty.

--- src/main/java/org/simplecoin/Block.java
+++ src/main/java/org/simplecoin/Block.java
@@ -3,21 +3,37 @@ package org.simplecoin;
 import java.util.Date;
 
 public class Block {
-
     public String hash;
     public String previousHash;
     private String data;
     private long timeStamp;
+    private int nonce;
 
     public Block(String data, String previousHash){
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
+
         this.hash = calculateHash();
     }
+
     public String calculateHash(){
         String calculatedHash = StringUtil.applySha256(
-                previousHash + Long.toString(timeStamp) + data);
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data
+                        );
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty){
+        // Create a string with difficulty * "0"
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals(target)){
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block mined!! : " + hash);
     }
 }
--- src/main/java/org/simplecoin/SimpleChain.java
+++ src/main/java/org/simplecoin/SimpleChain.java
@@ -6,18 +6,31 @@ import java.util.ArrayList;
 
 public class SimpleChain {
     public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 10;
     public static void main(String[] args) {
         blockchain.add(new Block("first block", "0"));
+        System.out.println("Trying to Mine Block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
+
         blockchain.add(new Block("second block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to Mine Block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
+
         blockchain.add(new Block("third block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to Mine Block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockChain is Valid: " + isChainValid());
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
         System.out.println(blockchainJson);
     }
 
     public static boolean isChainValid(){
         Block currentBlock;
         Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
         for(int i = 1; i <blockchain.size(); ++i){
             currentBlock = blockchain.get(i);
@@ -28,6 +41,10 @@ public class SimpleChain {
             }
             if(!previousBlock.hash.equals(currentBlock.previousHash)){
                 System.out.println("Previous Hashes not equal");
+                return false;
+            }
+            if(!currentBlock.hash.substring(0, difficulty).equals(hashTarget)){
+                System.out.println("This block hasn't been mined");
                 return false;
             }
         }
